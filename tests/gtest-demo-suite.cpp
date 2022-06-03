37c9e0cbf97d6060b614ac6767c1b64585db8c41
Joerg Faschingbauer <jf@faschingbauer.co.at>
2022-06-03 11:26:17 +0200

    OO introduced

--- tests/gtest-demo-suite.cpp
+++ tests/gtest-demo-suite.cpp
@@ -3,4 +3,24 @@
 
 TEST(gtest_demo_suite, passes)
 {
+    int a = 1;
+    int b = 1;
+
+    ASSERT_EQ(a, b);
+}
+
+// TEST(gtest_demo_suite, fails)
+// {
+//     int a = 1;
+//     int b = 2;
+
+//     ASSERT_EQ(a, b);
+// }
+
+TEST(gtest_demo_suite, passes_nocheinmal)
+{
+    int a = 1;
+    int b = 2;
+
+    ASSERT_NE(a, b);
 }
